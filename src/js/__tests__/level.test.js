e14134b1ce6692a0b8833d3ca014534f677fb77b
GreyRuler <daniil_vasilenko757@mail.ru>
2022-09-11 18:29:25 +0300

    impl

---
+++ src/js/__tests__/level.test.js
@@ -0,0 +1,29 @@
+import getLevel from "../level";
+import fetchData from "../http";
+
+jest.mock('../http')
+
+beforeAll(() => {
+    jest.resetAllMocks()
+})
+
+test('should get information about the level ', () => {
+    const response = {
+        status: 'ok',
+        level: 4
+    }
+    fetchData.mockReturnValue(response)
+
+    const result = getLevel(1)
+    expect(result).toBe("Ваш текущий уровень: 4")
+})
+
+test('should get information about unavailability ', () => {
+    const response = {
+        status: 'error'
+    }
+    fetchData.mockReturnValue(response)
+
+    const result = getLevel(1)
+    expect(result).toBe("Информация об уровне временно недоступна")
+})
